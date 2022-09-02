521cc80617062979041c62aac01efb7729f94ed0
smurtee <muratees@yahoo.com>
2022-09-01 22:05:21 -0500

    it works!

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let generateBtn = document.querySelector("#generate");
 
 let lowerCaseAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let UpperCaseAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let specialCharacter = [ '!', '@', '#', '$', '%', '&', '*'] //special character options
-let numericCharacter =[ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] //number options
+let specialCharacter = ['!', '@', '#', '$', '%', '&', '*'] //special character options
+let numericalCharacter = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'] //number options
 
 
 // Write password to the #password input
@@ -16,18 +16,43 @@ function writePassword() {
 
 }
 
-function generatePassword(){
-    
+function generatePassword() {
+  let characterLimit = prompt("How many characters (8-128)?")
+
+  let userOptions = [];
+
+  let allowsForLowerCase = confirm("Do you want to include lowercase?")
+  if (allowsForLowerCase) {
+    userOptions = userOptions.concat(lowerCaseAlphabet);
+
+  }
+  let allowsForUpperCase = confirm("Do you want to include UPPERCASE?")
+  if (allowsForUpperCase) {
+    userOptions = userOptions.concat(UpperCaseAlphabet);
+
+  }
+  let allowsForNumericalCharacters = confirm("Do you want to include numerical characters?")
+  if (allowsForNumericalCharacters) {
+    userOptions = userOptions.concat(numericalCharacter);
+
+  }
+  let allowsForSpecialCharacters = confirm("Do you want to include special characters?")
+  if (allowsForSpecialCharacters) {
+    userOptions = userOptions.concat(specialCharacter)
+
+  }
 
   let password = "";
-    for (let i = 0; i < 100; i++) {
-        let randomNumber = Math.floor(Math.random()*lowercaseAlphabet.length);
-        let randomLetter = lowercaseAlphabet[randomNumber];
-        password +=  randomLetter;
 
-    }
+  if ((userOptions.length > 0) && (characterLimit >= 8) && (characterLimit <=128))
+  for (let i = 0; i < characterLimit; i++) {
+    let randomNumber = Math.floor(Math.random() * userOptions.length);
+    let randomLetter = userOptions[randomNumber];
+    password += randomLetter
 
-    return password;
+  }
+
+  return password;
 
 
 }
